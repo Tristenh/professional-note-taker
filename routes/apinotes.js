57002bb91a624006641c9e52533e7e919ee837ea
Tristenh <tristenhobba1@gmail.com>
2023-08-01 16:30:03 +1000

    Added UUID generator, post apiNotes title and text to req.body, create newNote with title text and UUID, convert the json to js object, parsedNotes is then converted back to json and written to db.json

--- routes/apinotes.js
+++ routes/apinotes.js
@@ -1,4 +1,5 @@
 const apiNotes = require("express").Router();
+const uuid = require("../helpers/uuid");
 
 const fs = require("fs");
 
@@ -8,4 +9,42 @@ apiNotes.get("/", (req, res) => {
   res.json(parsedNotes);
 });
 
+apiNotes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 2),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated note!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
 module.exports = apiNotes;
