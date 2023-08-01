ddd8b50ca4791de2a6f1cf3b5968b582ba20c08c
Tristenh <tristenhobba1@gmail.com>
2023-08-01 17:11:03 +1000

    added procfile for heroku deployment , fix error in js file port to capitalised PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const api = require("./routes/index");
 
