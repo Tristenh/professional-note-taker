1664269daa1bdd416756b35e05b22a862f852a51
Tristenh <tristenhobba1@gmail.com>
2023-08-01 16:41:04 +1000

    commented file, added more example notes

--- routes/apinotes.js
+++ routes/apinotes.js
@@ -1,14 +1,13 @@
 const apiNotes = require("express").Router();
 const uuid = require("../helpers/uuid");
-
 const fs = require("fs");
-
+// getroute for /
 apiNotes.get("/", (req, res) => {
   const data = fs.readFileSync("./db/db.json");
   const parsedNotes = JSON.parse(data);
   res.json(parsedNotes);
 });
-
+//  posting /
 apiNotes.post("/", (req, res) => {
   const { title, text } = req.body;
 
@@ -18,13 +17,14 @@ apiNotes.post("/", (req, res) => {
       text,
       id: uuid(),
     };
+    // reading db.json data
     fs.readFile("./db/db.json", (err, data) => {
       if (err) {
         console.error(err);
       } else {
         const parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
-
+        //  writing new data to db.json
         fs.writeFile(
           "./db/db.json",
           JSON.stringify(parsedNotes, null, 2),
@@ -35,7 +35,7 @@ apiNotes.post("/", (req, res) => {
         );
       }
     });
-
+    // response
     const response = {
       status: "success",
       body: newNote,
